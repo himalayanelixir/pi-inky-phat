95793277a7da98e7ba34a60f4a98f8d50b15b407
Harlen Bains <himalayanelixir@gmail.com>
2020-04-12 20:47:53 -0700

    added display.py and firstboot.sh

---
+++ display.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+# Copyright 2020 Harlen Bains
+# linted using pylint
+# formatted using black
+"""This script takes in one agrgument and then displays it on a InkyPhat
+display"""
+import sys
+from PIL import Image, ImageFont, ImageDraw  # pylint: disable=import-error
+from font_fredoka_one import FredokaOne  # pylint: disable=import-error
+from inky import InkyPHAT  # pylint: disable=import-error
+
+
+def print_text(text):
+    """Displays text that is passed as an argument"""
+    inky_display = InkyPHAT("yellow")
+    inky_display.set_border(inky_display.WHITE)
+    img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype(FredokaOne, 30)
+    message = text
+    width, height = font.getsize(message)
+    x_axis = (inky_display.WIDTH / 2) - (width / 2)
+    y_axis = (inky_display.HEIGHT / 2) - (height / 2)
+    draw.text((x_axis, y_axis), message, inky_display.YELLOW, font)
+    inky_display.set_image(img)
+    inky_display.show()
+
+
+if __name__ == "__main__":
+    try:
+        print_text(str(sys.argv[1]))
+    except IndexError:
+        print_text("No Message")
---
+++ image-setup/firstboot.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# gives a bit of time for the wifi to connect
+sleep 20
+# enable ssh
+sudo systemctl enable ssh
+# update and upgrade 
+sudo apt-get update -y
+sudo apt-get upgrade -y
+# install programs
+sudo apt-get install expect git zsh python3-pip python3-venv -y
+# change default shell for root and pi users
+sudo chsh -s /bin/zsh pi
+sudo chsh -s /bin/zsh
+# install ohmyzsh for root and pi users
+sudo -u pi sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+
+# install font for display
+pip3 install font_fredoka_one
+
+
+# install drivers for inkyphat screen
+wget https://get.pimoroni.com/inky -P /home/pi
+# make downloaded script executable
+chmod +x /home/pi/inky
+# create script that uses expect to automate adafruit installation prompts
+cat <<EOT >/home/pi/script.exp
+#!/usr/bin/expect -f
+set timeout -1
+spawn ./inky
+match_max 100000
+expect -exact "Do you wish to continue? \[y/N\] "
+send -- "y\r"
+expect -exact "Do you wish to perform a full install? \[y/N\] "
+send -- "N\r"
+expect eof
+EOT
+# make script executable 
+chmod +x /home/pi/script.exp
+# run automation script which installs the adafruit screen drivers
+chown pi /home/pi/script.exp
+chown pi /home/pi/inky
+su -c '/home/pi/script.exp' - pi
+# remove the screen installation script
+rm /home/pi/inky
+# remove expect automation script
+rm /home/pi/script.exp
+
+
+# download the display program from github
+wget -P /home/pi https://raw.githubusercontent.com/himalayanelixir/alexa-pi-motor-control/master/display.py
+# make display program executable
+sudo chmod +x /home/pi/display.py
+# add display program to PATH
+echo "export PATH=\"/home/pi:$PATH\"" >>/home/pi/.zshrc
+# make pi user owner of the file we downloaded
+chown pi /home/pi/display.py
+# print success message
+python3 /home/pi/display.py $(date +"%T")   
+
+
+# tell pi to restart after one minute. This is needed for the ssh changes to work and for the adafruit screen drivers
+sudo shutdown -r 1
