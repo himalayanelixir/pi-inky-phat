50a7eec25a4d76020f6d23d466f8ec0504586e4a
Harlen Bains <himalayanelixir@gmail.com>
2020-04-22 08:08:35 -0700

    moving install to new script

--- image-setup/firstboot.sh
+++ image-setup/firstboot.sh
@@ -17,48 +17,12 @@ sudo -u pi sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh
 sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 
-# install font for display
-pip3 install font_fredoka_one
-
-
-# install drivers for inkyphat screen
-wget https://get.pimoroni.com/inky -P /home/pi
-# make downloaded script executable
-chmod +x /home/pi/inky
-# create script that uses expect to automate adafruit installation prompts
-cat <<EOT >/home/pi/script.exp
-#!/usr/bin/expect -f
-set timeout -1
-spawn ./inky
-match_max 100000
-expect -exact "Do you wish to continue? \[y/N\] "
-send -- "y\r"
-expect -exact "Do you wish to perform a full install? \[y/N\] "
-send -- "N\r"
-expect eof
-EOT
-# make script executable 
-chmod +x /home/pi/script.exp
-# run automation script which installs the adafruit screen drivers
-chown pi /home/pi/script.exp
-chown pi /home/pi/inky
-su -c '/home/pi/script.exp' - pi
-# remove the screen installation script
-rm /home/pi/inky
-# remove expect automation script
-rm /home/pi/script.exp
-
-
-# download the display program from github
-wget -qP /home/pi https://raw.githubusercontent.com/himalayanelixir/pi-inky-phat/master/display.py
-# make display program executable
-sudo chmod +x /home/pi/display.py
-# add display program to PATH
-echo "export PATH=\"/home/pi:$PATH\"" >>/home/pi/.zshrc
-# make pi user owner of the file we downloaded
-chown pi /home/pi/display.py
-# print success message
-python3 /home/pi/display.py $(date +"%T")   
+# download Inky pHAT installer script
+wget -qP /home/pi https://raw.githubusercontent.com/himalayanelixir/pi-inky-phat/master/inky-phat-installer.sh
+# make executeable
+chmod +x /home/pi/inky-phat-installer.sh
+# run script
+sh /home/pi/inky-phat-installer.sh
 
 
 # tell pi to restart after one minute. This is needed for the ssh changes to work and for the adafruit screen drivers
